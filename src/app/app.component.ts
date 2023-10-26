af27456604431a8caa78286af5b3f89e5cfb6967
wecom tech <wecomtech22@gmail.com>
2023-10-26 09:30:01 +0530

    complete minimum bets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,17 +24,22 @@ export class AppComponent {
   profitValue: number = this.inputValue * this.singleData.price; // Calculate profitValue
   minimumBets:any;
   showSpinner: boolean = true;
+  generatedId:string='';
+  
   constructor(private renderer: Renderer2, private el: ElementRef,private http:HttpClient) {}
   
 
   ngOnInit(): void {
     this.getData();
     this.startTimer();
-    this.getMinimumBets();
-    this.spinwheel2()
+    // this.getMinimumBets();
+    // this.spinwheel2()
   }
 
-
+  // interface ApiResponse {
+  //   message: string;
+  //   generatedId: string; // Ensure that this matches the property in your API response
+  // }
   // this.profitValue = (this.inputValue * this.singleData.price);
   getButtonValue(value:number,event:Event){
     event.preventDefault();
@@ -44,65 +49,76 @@ export class AppComponent {
   startTimer() {
     const intervalId = setInterval(() => {
         if (this.remainingTime === 60) {
-            const generateId = Math.ceil(Math.random() * 20);
-            console.log("generated Id:", generateId);
-        }
+            // const generatedId = Math.ceil(Math.random() * 20);
+            // console.log("generated Id:", generatedId);
+            const timestamp = new Date().getTime().toString(); // Get current timestamp as a string
+            const randomString = Math.random().toString(36).substring(2, 8); // Generate a random string
+            const uniqueId = `${timestamp}-${randomString}`;
+            this.generatedId = uniqueId
+            // this.getMinimumBets()
+            // console.log(uniqueId)
+            }
         this.remainingTime--;
         if (this.remainingTime <= 10 && !this.isButtonDisabled) {
             this.isButtonDisabled = true;
+            this.getMinimumBets()
+            // console.log(this.minimumBets.field)
             this.spinwheel2();
         }
         if (this.remainingTime < 0) {
-            this.remainingTime = 60;
             this.isButtonDisabled = false;
-            clearInterval(intervalId);
+            this.remainingTime = 60;
             console.log('Timer expired!');
         }
     }, 1000);
 }
 
 
-  isSpinning: boolean = false;
+  // isSpinning: boolean = false;
 
-  updateButtons(){
-    if(this.remainingTime <= 10 && !this.isSpinning){
-      // this.isSpinning = true;
-      // this.spinwheel()
-      this.spinwheel2()
-      this.isButtonDisabled = true;
-      // setTimeout(() => {
-      //   this.isSpinning = false;
-      // }, 10000);
-    }else{
-      this.isButtonDisabled = false;
-    }
-  }
+  // updateButtons(){
+  //   if(this.remainingTime <= 10 && !this.isSpinning){
+  //     // this.isSpinning = true;
+  //     // this.spinwheel()
+  //     this.spinwheel2()
+  //     this.isButtonDisabled = true;
+  //     // setTimeout(() => {
+  //     //   this.isSpinning = false;
+  //     // }, 10000);
+  //   }else{
+  //     this.isButtonDisabled = false;
+  //   }
+  // } 
 
   spinwheel2() {
-    const startTime = performance.now();
-    const duration = 10000; // 10 seconds in milliseconds
     const wheel = this.el.nativeElement.querySelector(".wheel");
-    this.spinWheelAnimation(wheel, startTime, duration);
-  }
-  
-  spinWheelAnimation(wheel: any, startTime: number, duration: number) {
-    const currentTime = performance.now();
-    const elapsedTime = currentTime - startTime;
-  
-    if (elapsedTime < duration) {
-      const randomValue = Math.floor(Math.random() * 10000);
-      this.renderer.setStyle(wheel, "transform", `rotate(${randomValue}deg)`);
-      requestAnimationFrame(() => this.spinWheelAnimation(wheel, startTime, duration));
-    } else {
-      // After 10 seconds, calculate stop angle and stop the wheel
-      const field = 'tiger'
-      const stopAngle = this.stopAtAngle(field);
-      this.renderer.setStyle(wheel, "transition", "none");
-      this.renderer.setStyle(wheel, "transform", `rotate(${stopAngle}deg)`);
-    }
-  }
-  
-  stopAtAngle(field: string): number {
+    const initialSpinDuration = 2000; // 2 seconds for initial spin
+    const stopDelay = 8000; // 8 seconds after the initial spin for stopping at the correct position
+
+    // Initial spin (for 2 seconds)
+    const randomValue = Math.floor(Math.random() * 10000);
+    this.renderer.setStyle(wheel, "transition", `transform ${initialSpinDuration / 1000}s ease-out`);
+    this.renderer.setStyle(wheel, "transform", `rotate(${randomValue}deg)`);
+
+    // After initial spin duration, stop the wheel
+    setTimeout(() => {
+        this.stopWheel(wheel, stopDelay);
+    }, initialSpinDuration);
+}
+
+stopWheel(wheel: any, stopDelay: number) {
+  // this.getMinimumBets()
+    const field = this.minimumBets.field
+    console.log(this.minimumBets.field)
+
+    const stopAngle = this.stopAtAngle(field);
+    
+    // Apply transition and stop the wheel
+    this.renderer.setStyle(wheel, "transition", `transform ${stopDelay / 1000}s ease-out`);
+    this.renderer.setStyle(wheel, "transform", `rotate(${stopAngle}deg)`);
+}
+
+stopAtAngle(field: string): number {
     let stopAngle: number;
 
     // Logic to calculate stop angle based on the field
@@ -117,12 +133,40 @@ export class AppComponent {
 
     } else {
         // Handle other fields if needed
-        stopAngle = 90/* default stop angle if field is not recognized */;
+        stopAngle = 45/* default stop angle if field is not recognized */;
     }
-    console.log("return stopAngel:",stopAngle)
+    // console.log("return stopAngel:",stopAngle)
 
     return stopAngle;
 }
+
+  
+// spinwheel2() {
+//     const startTime = performance.now();
+//     const duration = 10000; // 10 seconds in milliseconds
+//     const wheel = this.el.nativeElement.querySelector(".wheel");
+//     this.spinWheelAnimation(wheel, startTime, duration);
+//   }
+  // spinWheelAnimation(wheel: any, startTime: number, duration: number) {
+  //   const currentTime = performance.now();
+  //   const elapsedTime = currentTime - startTime;
+  
+  //   if (elapsedTime < duration) {
+  //     const randomValue = Math.floor(Math.random() * 10000);
+  //     this.renderer.setStyle(wheel, "transform", `rotate(${randomValue}deg)`);
+  //     requestAnimationFrame(() => this.spinWheelAnimation(wheel, startTime, duration));
+  //   } else {
+  //     // After 10 seconds, calculate stop angle and stop the wheel
+  //     const field = this.minimumBets.field
+  //     const stopAngle = this.stopAtAngle(field);
+  //     this.renderer.setStyle(wheel, "transition", "none");
+  //     this.renderer.setStyle(wheel, "transform", `rotate(${stopAngle}deg)`);
+  //   }
+  // }
+  
+
+  
+  
 
 
 //   spinwheel() {
@@ -171,11 +215,13 @@ export class AppComponent {
   addBets(){
     let formdata = new FormData()
      
-    formdata.append("price",this.singleData.userPrice)
+    formdata.append("price",String(this.inputValue))
     formdata.append("id",this.singleData.id)
+    formdata.append("generatedId",this.generatedId)
 
-    this.http.post("http://localhost:3100/addPrice",formdata).subscribe(res=>{
+    this.http.post("http://localhost:3100/addPrice",formdata).subscribe((res)=>{
       console.log(res)
+      // this.generatedId = res.generatedId;
     })
   }
 
@@ -215,7 +261,9 @@ export class AppComponent {
   }
 
   getMinimumBets(){
-    this.http.get("http://localhost:3100/showBetsData").subscribe(res=>{
+    let id = this.generatedId
+    // console.log(id)
+    this.http.get("http://localhost:3100/showBetsData/" + id).subscribe(res=>{
       this.minimumBets = res
       console.log(this.minimumBets)
     })
